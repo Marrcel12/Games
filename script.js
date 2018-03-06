bf7940e32a94702e58834f92205e7664e1c16ad3
Unknown <marrcel12.marga@gmail.com>
2018-03-06 20:19:33 +0100

    init

---
+++ script.js
@@ -0,0 +1,36 @@
+var player = 1; //definuje który gracz zaczyna 
+
+$(".pole").click(function () {
+    if (player == 1) {
+        $(this).html("X");
+        $(this).unbind( "click" );
+        player++
+        check("X")
+    }
+    else{
+        $(this).html("O");
+        $(this).unbind( "click" );
+        player--
+        check("O")
+    }
+}); //mechanizm przełączania gra
+
+
+//mechanizm sprawdzania który gracz wygrał
+
+function check(a) {
+    
+    if( $("#a").html()===a &&  $("#b").html()===a && $("#c").html()===a || 
+        $("#a").html()===a &&  $("#d").html()===a && $("#g").html()===a ||
+        $("#a").html()===a &&  $("#e").html()===a && $("#i").html()===a ||
+        $("#c").html()===a &&  $("#f").html()===a && $("#i").html()===a ||
+        $("#a").html()===a &&  $("#d").html()===a && $("#g").html()===a ||
+        $("#d").html()===a &&  $("#e").html()===a && $("#f").html()===a ||
+        $("#g").html()===a &&  $("#h").html()===a && $("#i").html()===a ||
+        $("#g").html()===a &&  $("#e").html()===a && $("#c").html()===a
+      ){
+        alert(a+"wygrał ");
+    }
+    
+    
+}
